895668b2cf22a21b1de6d134478a06ce2437f0e1
SIDNEY SILVA <67187745+sxdny@users.noreply.github.com>
2024-01-11 21:19:18 +0100

    Retocar el Memory
    Todo funciona

--- asignaturas/diw/act2jQuery/script.js
+++ asignaturas/diw/act2jQuery/script.js
@@ -52,9 +52,8 @@ let cards = [
 
 // imprimir el juego en el DOM
 $("document").ready(() => {
-
-    // variable para guardar el par de cartas seleccionadas
-    let selectedCards = [2];
+  // variable para guardar el par de cartas seleccionadas
+  let selectedCards = [2];
 
   cards = shuffle(cards);
   cards = cortarArray(cards);
@@ -75,40 +74,90 @@ $("document").ready(() => {
 
   cards.forEach((card) => {
     $("#game").append(
-      `<div class="card" data-card-name="${card.name}"><div class="back" name="${card.img}"></div><div class="front" style="background: url('img/${card.img}') no-repeat center center; background-size: cover"></div></div>`
+      `<div class="card" guessed="false" fliped="false" data-card-name="${card.name}"><div class="back" name="${card.img}"></div><div class="front" style="background: url('img/${card.img}') no-repeat center center; background-size: cover"></div></div>`
     );
   });
-    
-    // hacer que cuando haga click en la carta se muestre la imagen
-    $(".back").on("click", function () {
-        // ponemos un display none a la carta
-        $(this).css("display", "none");
-        // le quitamos el display none a la carta de delante
-        $(this).next().css("display", "block");
 
-        // guardamos la carta seleccionada en la variable selectedCards
-        selectedCards.push($(this).parent().attr("data-card-name"));
+  // hacer que cuando haga click en la carta se muestre la imagen
+  $(".back").on("click", function () {
+    // cambiamos el estado de la carta a true
+    $(this).parent().attr("fliped", "true");
 
-        // si la variable tiene dos cartas
-        if (selectedCards.length === 3) {
-            // comprobamos si las cartas son iguales
-            if (selectedCards[0] == selectedCards[1]) {
-                // si son iguales las dejamos boca arriba
-                $(".back").css("display", "none");
-                $(".front").css("display", "block");
-            } else {
-                // si no son iguales las volvemos a poner boca abajo
+    // ponemos un display none a la carta
+    $(this).css("display", "none");
+    // le quitamos el display none a la carta de delante
+    $(this).next().css("display", "block");
 
-                // TODO Ponerle id a las cartas para poder seleccionarlas y hacer el manejo
-
-                $(".back").css("display", "block");
-                $(".front").css("display", "none");
-
-                // borramos las cartas de la variable
-                selectedCards = [];
-            }
-        }
+    // guardamos las cartas con fliped true en la variable
+    selectedCards = $(`div[fliped="true"]`).map(function () {
+      return $(this).attr("data-card-name");
     });
+
+    // mostramos las cartas seleccionadas
+    console.log(selectedCards);
+
+    // si la variable tiene dos cartas
+    if (selectedCards.length === 2) {
+      // comprobamos si las cartas son iguales
+      if (selectedCards[0] == selectedCards[1]) {
+        // si son iguales las dejamos boca arriba
+
+        // obtenemos el data-card-name de la carta seleccionada
+        let cardName = $(this).parent().attr("data-card-name");
+
+        // seleccionamos las cartas con el mismo data-card-name
+        $(`div[data-card-name="${cardName}"][fliped="true"]`).css(
+          "display",
+          "block"
+        );
+
+        // cambiamos el estado guessed a true
+        $(`div[data-card-name="${cardName}"][fliped="true"]`).attr(
+          "guessed",
+          "true"
+        );
+
+        // cambiamos el estado fliped a false
+        $(`div[data-card-name="${cardName}"][fliped="true"]`).attr(
+          "fliped",
+          "false"
+        );
+
+        // borramos las cartas de la variable
+        selectedCards = [];
+      } else {
+        // si no son iguales las volvemos a poner boca abajo
+
+        // obtenemos el data-card-name de las dos cartas seleccionadas
+        let cardName1 = selectedCards[0];
+        let cardName2 = selectedCards[1];
+
+        // seleccionamos las cartas con el mismo data-card-name
+        let card1 = $(`div[data-card-name="${cardName1}"][fliped="true"]`);
+        let card2 = $(`div[data-card-name="${cardName2}"][fliped="true"]`);
+
+        console.log(card1);
+        console.log(card2);
+
+        let children1 = $(card1).children();
+
+        let children2 = $(card2).children();
+
+        children1[0].style.display = "block";
+        children1[1].style.display = "none";
+
+        children2[0].style.display = "block";
+        children2[1].style.display = "none";
+
+        // cambiamos el estado fliped a false
+        card1.attr("fliped", "false");
+        card2.attr("fliped", "false");
+
+        // borramos las cartas de la variable
+        selectedCards = [];
+      }
+    }
+  });
 });
 
 console.log("Hola Mundo!");
